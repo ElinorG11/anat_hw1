e8907216634165ac5fa15dca57ecfcba6d8ce750
elinor.g <elinor.g12@gmail.com>
2022-10-30 17:39:15 +0200

    Q4v1

--- code/main.py
+++ code/main.py
@@ -69,7 +69,7 @@ x_inv = np.fft.ifft2(np.fft.ifftshift(freq_ldirection))
 y_inv = np.fft.ifft2(np.fft.ifftshift(freq_kdirection))
 xy_inv = np.fft.ifft2(np.fft.ifftshift(freq_lkdirection))
 
-fig132, axes = plt.subplots(1, 3, figsize=(15,15))
+fig132, axes = plt.subplots(1, 3, figsize=(15, 15))
 axes[0].imshow(np.abs(x_inv), cmap='gray')
 axes[0].set_title('Grayscale of the building with vertical filter')
 axes[1].imshow(np.abs(y_inv), cmap='gray')
@@ -78,6 +78,7 @@ axes[2].imshow(np.abs(xy_inv), cmap='gray')
 axes[2].set_title('Grayscale of the building with vertical and horizontal filters')
 plt.tight_layout()
 plt.show()
+
 
 # Q1.d
 
@@ -97,7 +98,7 @@ def max_freq_filtering(fshift, precentege):
 
     unique_freq = np.unique(abs(fshift))
 
-    bound_maximal_freq = unique_freq[int(unique_freq.shape[0] * (1-precentege))]
+    bound_maximal_freq = unique_freq[int(unique_freq.shape[0] * (1 - precentege))]
 
     (row, col) = fshift.shape
     fMaxFreq = np.zeros((row, col), dtype=complex)
@@ -108,6 +109,7 @@ def max_freq_filtering(fshift, precentege):
     imgMaxFreq = np.fft.ifft2(np.fft.ifftshift(fMaxFreq))
 
     return fMaxFreq, imgMaxFreq
+
 
 max_10_freq, building_img_mex10freq = max_freq_filtering(fft_gray_building, 10)
 fig3, axes = plt.subplots(2, 1, figsize=(10, 10))
@@ -131,11 +133,11 @@ plt.show()
 # Q1.f
 # as p gets bigger - we include more and more frequencies from the original image, thus the filtered image is closer
 # to the original one and the MSE will be smaller
-laxe= np.arange(1, 101)
-kaxe= np.zeros(100)
+laxe = np.arange(1, 101)
+kaxe = np.zeros(100)
 for p in range(1, 101):
     _, img_max_freq = max_freq_filtering(fft_gray_building, p)
-    kaxe[p-1] = np.square(np.subtract(gray_building, np.abs(img_max_freq))).mean()
+    kaxe[p - 1] = np.square(np.subtract(gray_building, np.abs(img_max_freq))).mean()
 plt.title('MSE', fontsize=10)
 plt.xlabel('p value', fontsize=10)
 plt.ylabel('MSE(p)', fontsize=10)
@@ -166,16 +168,16 @@ phase_portrait = np.angle(np.fft.fftshift(np.fft.fft2(gray_portrait)))
 amp_portrait = np.abs(np.fft.fftshift(np.fft.fft2(gray_portrait)))
 
 fig22, axes = plt.subplots(1, 2, figsize=(10, 10))
-axes[0].imshow(np.log(1+np.abs(amp_parrot)), cmap='gray')
+axes[0].imshow(np.log(1 + np.abs(amp_parrot)), cmap='gray')
 axes[0].set_title("gray parrot amplitude")
-axes[1].imshow(np.log(1+np.abs(amp_portrait)), cmap='gray')
+axes[1].imshow(np.log(1 + np.abs(amp_portrait)), cmap='gray')
 axes[1].set_title("gray portrait amplitude")
 plt.tight_layout()
 plt.show()
 
 # Q2.c
-ampPort_phaseParrot = np.multiply(amp_portrait, np.exp(1j*phase_parrot))
-ampParrot_phasePort = np.multiply(amp_parrot, np.exp(1j*phase_portrait))
+ampPort_phaseParrot = np.multiply(amp_portrait, np.exp(1j * phase_parrot))
+ampParrot_phasePort = np.multiply(amp_parrot, np.exp(1j * phase_portrait))
 
 fig23, axes = plt.subplots(1, 2, figsize=(10, 10))
 axes[0].imshow(np.abs(np.fft.ifft2(np.fft.ifftshift(ampPort_phaseParrot))), cmap='gray')
@@ -189,8 +191,8 @@ plt.show()
 random_phase = np.random.uniform(low=-np.pi, high=np.pi, size=gray_portrait.shape)
 random_amp = np.random.uniform(low=0, high=100, size=gray_portrait.shape)
 
-ampPort_phaseRdm = np.multiply(amp_portrait, np.exp(1j*random_phase))
-ampRdm_phasePort = np.multiply(random_amp, np.exp(1j*phase_portrait))
+ampPort_phaseRdm = np.multiply(amp_portrait, np.exp(1j * random_phase))
+ampRdm_phasePort = np.multiply(random_amp, np.exp(1j * phase_portrait))
 
 fig24, axes = plt.subplots(1, 2, figsize=(10, 10))
 axes[0].imshow(np.abs(np.fft.ifft2(np.fft.ifftshift(ampPort_phaseRdm))), cmap='gray')
@@ -200,3 +202,86 @@ axes[1].set_title("Portrait Phase with Random Amplitude")
 plt.tight_layout()
 plt.show()
 
+
+# --------------------------------------------------Q4-------------------------------------------------- #
+
+# Q4.a
+def bilinear_displacement(dx, dy, image):
+    """
+    Calculate the displacement of a pixel using a bilinear interpolation.
+    :param dx: the displacement in the x direction. dx in rang [0,1).
+    :param dy: the displacement in the y direction. dy in rang [0,1).
+    :param image: The image on which we preform the cyclic displacement
+    :return:
+    displaced_image: The new displaced image
+    """
+
+    (rows, cols) = np.shape(image)
+
+    identity_x = np.identity(rows)
+
+    vec_x = np.zeros((rows, rows))
+    i, j = np.indices(vec_x.shape)
+    vec_x[i == j - 1] = 1
+
+    identity_y = np.identity(cols)
+
+    vec_y = np.zeros((cols, cols))
+    i, j = np.indices(vec_y.shape)
+    vec_y[i == j + 1] = 1
+
+    move_dx = (1 - dx) * identity_x + dx * vec_x
+    move_dy = (1 - dy) * identity_y + dy * vec_y
+
+    displaced_image = np.matmul(np.matmul(move_dx, image), move_dy)
+    print(displaced_image)
+    return displaced_image
+
+
+def general_displacement(dx, dy, image):
+    """
+    Calculate the displacement of a pixel using a bilinear interpolation.
+    :param dx: the displacement in the x direction.
+    :param dy: the displacement in the y direction.
+    :param image: The image on which we preform the cyclic displacement
+    :return:
+    displaced_image: The new displaced imag
+    """
+
+    (rows, cols) = np.shape(image)
+    dx_fraction = float('%0.2f' % (dx % 1))
+    dx_int = int(dx)
+    dy_fraction = float('%0.2f' % (dy % 1))
+    dy_int = int(dy)
+
+    # shift by integer value
+    displaced_x_image = np.zeros((np.shape(image)))  # x shift
+    if dx_int >= 0:
+        displaced_x_image[dy_int:, :] = image[:rows-dy_int, :]
+    else:
+        displaced_x_image[0:rows-dy_int, :] = image[dy_int:, :]
+
+    displaced_y_image = np.zeros((np.shape(image)))  # y shift
+    if dy_int >= 0:
+        displaced_y_image[:, dx_int:] = displaced_x_image[:, 0:cols-dx_int]
+    else:
+        displaced_y_image[:, 0:cols-dx_int] = displaced_x_image[:, dx_int:]
+
+    # shift by non-natural value
+    displaced_image = bilinear_displacement(dx_fraction, dy_fraction, displaced_y_image)
+    return displaced_image
+
+
+cameraman = cv2.imread(filename='../given_data/cameraman.jpg')
+
+gray_cameraman = cv2.cvtColor(cameraman, cv2.COLOR_BGR2GRAY)
+
+displaced_cameraman = general_displacement(150.7, 110.4, gray_cameraman)
+
+fig43, axes = plt.subplots(1, 2, figsize=(10, 10))
+axes[0].imshow(gray_cameraman, cmap='gray')
+axes[0].set_title("original cameraman")
+axes[1].imshow(displaced_cameraman)
+axes[1].set_title("displaced cameraman by [150.7, 110.4]")
+plt.tight_layout()
+plt.show()
